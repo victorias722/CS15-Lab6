e21029017ef569cf4f887e9435901fd308e4ecb4
victorias722 <119906318+victorias722@users.noreply.github.com>
2023-04-12 14:54:55 -0400

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,28 @@
+package lab6;
+
+/**
+ ____                      _              _ _ _     _
+ |  _ \  ___    _ __   ___ | |_    ___  __| (_) |_  | |
+ | | | |/ _ \  | '_ \ / _ \| __|  / _ \/ _` | | __| | |
+ | |_| | (_) | | | | | (_) | |_  |  __/ (_| | | |_  |_|
+ |____/ \___/  |_| |_|\___/ \__|  \___|\__,_|_|\__| (_)
+ This is support code for the lab. You may look at it if you are interested,
+ but you will not have to change anything to complete your lab.
+ */
+
+/**
+ * Constants class for the lab.
+ */
+public class Constants {
+    // Panel constants
+    public static final int PANEL_WIDTH = 600;
+    public static final int PANEL_HEIGHT = 600;
+    public static final int CP_HEIGHT = 40;
+    public static final int PADDING = 5;
+    public static final int RANDOM_LOC_BOUND = 100;
+    public static final int EYE_RADIUS = 4;
+    public static final int BODY_RADIUS = 20;
+    public static final int EYE_X_OFFSET = 5;
+    public static final int EYE_Y_OFFSET = 7;
+
+}
